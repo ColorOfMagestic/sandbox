7831a3dabb1a6472f34928c6499431c9f7a5e473
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-03-17 14:29:10 +0500

    инициализация слайдера

---
+++ src/js/main/section/feedback.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".feedback-swiper", {
+  loop: true,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
